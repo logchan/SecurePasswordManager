2005b5569ffe8dcca39082d6da64a49701002081
logchan <loliconlg@gmail.com>
2015-01-07 17:51:18 +0800

    fixed passwordutility test

--- app/src/androidTest/java/hk/ust/ustac/team8/securepasswordmanager/PasswordUtilityTest.java
+++ app/src/androidTest/java/hk/ust/ustac/team8/securepasswordmanager/PasswordUtilityTest.java
@@ -1,6 +1,5 @@
 package hk.ust.ustac.team8.securepasswordmanager;
 
-import hk.ust.ustac.team8.passwordutility.AdditionalSaltingInformation;
 import hk.ust.ustac.team8.passwordutility.BasicSaltAppender;
 import hk.ust.ustac.team8.passwordutility.HashingPasswordGenerator;
 import hk.ust.ustac.team8.passwordutility.MD5HashingServiceProvider;
@@ -13,7 +12,7 @@ import android.test.InstrumentationTestCase;
  */
 public class PasswordUtilityTest extends InstrumentationTestCase {
 
-    public void testGenerator() {
+    public void testGenerator() throws Exception{
         MD5HashingServiceProvider md5Provider = new MD5HashingServiceProvider();
         NoEffectStringTransformer nonTransform = new NoEffectStringTransformer();
         BasicSaltAppender salt1 = new BasicSaltAppender("abc");
@@ -22,7 +21,7 @@ public class PasswordUtilityTest extends InstrumentationTestCase {
         HashingPasswordGenerator generator = new HashingPasswordGenerator(md5Provider, nonTransform);
         generator.addSalt(salt1);
         generator.addSalt(salt2);
-        
+
         String pwd = generator.generatePassword("somesecret", 42);
 
         assertEquals("87ee18deceb425c475aa334650b9f9b6", pwd);

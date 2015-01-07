d4c10981d4046d3c9b07d722a30fecbdd53ffac7
logchan <loliconlg@gmail.com>
2015-01-07 17:59:32 +0800

    updated langutility tests

--- app/src/androidTest/java/hk/ust/ustac/team8/securepasswordmanager/LangUtilityTest.java
+++ app/src/androidTest/java/hk/ust/ustac/team8/securepasswordmanager/LangUtilityTest.java
@@ -2,6 +2,7 @@ package hk.ust.ustac.team8.securepasswordmanager;
 
 import hk.ust.ustac.team8.generalutility.LangUtility;
 import hk.ust.ustac.team8.passwordutility.BasicSaltAppender;
+import hk.ust.ustac.team8.passwordutility.NoEffectStringTransformer;
 import hk.ust.ustac.team8.passwordutility.SaltingServiceProvider;
 
 import android.test.InstrumentationTestCase;
@@ -15,9 +16,24 @@ public class LangUtilityTest extends InstrumentationTestCase {
         BasicSaltAppender saltAppender = new BasicSaltAppender("abc");
         SaltingServiceProvider provider = (SaltingServiceProvider)saltAppender;
 
-        Object obj = LangUtility.getObjectOfSameClass(provider, new Object[] { "abc" });
+        Object obj = LangUtility.getObjectOfSameClass(provider, new Object[] {"abc"});
 
-        assertEquals(obj.getClass(), saltAppender.getClass());
-        assertEquals(((BasicSaltAppender)obj).getSalt(), saltAppender.getSalt());
+        assertEquals(saltAppender.getClass(), obj.getClass());
+        assertEquals(saltAppender.getSalt(), ((BasicSaltAppender)obj).getSalt());
+    }
+
+    public void testGetObjectOfSameClassWithDefaultConstructor() {
+        NoEffectStringTransformer transformer = new NoEffectStringTransformer();
+        Object obj = LangUtility.getObjectOfSameClass(transformer, new Object[] {});
+
+        assertEquals(transformer.getClass(), obj.getClass());
+    }
+
+    public void testGetObjectOfSameClassWithWrongArgs() {
+        BasicSaltAppender saltAppender = new BasicSaltAppender("abc");
+
+        Object obj = LangUtility.getObjectOfSameClass(saltAppender, new Object[] {"abc" , "def"});
+
+        assertEquals(null, obj);
     }
 }

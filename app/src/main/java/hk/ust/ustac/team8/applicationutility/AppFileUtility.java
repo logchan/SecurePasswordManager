05800bd5d6d0416684ba99fae49b694d4bbcb07f
logchan <loliconlg@gmail.com>
2015-01-10 10:57:29 +0800

    Done scheme reading & deleting

--- app/src/main/java/hk/ust/ustac/team8/applicationutility/AppFileUtility.java
+++ app/src/main/java/hk/ust/ustac/team8/applicationutility/AppFileUtility.java
@@ -1,8 +1,16 @@
 package hk.ust.ustac.team8.applicationutility;
 
 import hk.ust.ustac.team8.exception.SchemeNotFoundException;
+import hk.ust.ustac.team8.generalutility.FileUtility;
 import hk.ust.ustac.team8.hashingscheme.HashingScheme;
 
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.LinkedList;
 
 /**
@@ -17,16 +25,130 @@ public final class AppFileUtility {
 
     }
 
-    private void saveScheme(HashingScheme scheme) {
+    /**
+     * Get the directory for schemes. Will create if not exists. If a file of the same name exists,
+     * it will be deleted.
+     *
+     * @param context the app context
+     * @return the File object of the schemes dir
+     */
+    private File getSchemeDir(Context context) {
+        File dir = context.getFilesDir();
+        File sDir = new File(dir, "schemes");
+
+        if (!sDir.exists()) {
+            sDir.mkdir();
+        }
+        else if (!sDir.isDirectory()) {
+            sDir.delete();
+            sDir.mkdir();
+        }
+
+        return sDir;
+    }
+
+
+    public void saveScheme(HashingScheme scheme) {
         //TODO
     }
 
-    private LinkedList<HashingScheme> getAllSchemes() {
-        //TODO
-        return null;
+    /**
+     * Get a scheme from its directory. If the scheme has a name different from the dir name,
+     * the scheme will be renamed.
+     * The scheme will be read from file (dirname).hs
+     *
+     * @param dir the directory for the scheme
+     * @return the scheme object. null if no .hs found, or exception occurred when reading.
+     */
+    private HashingScheme getOneScheme(File dir) {
+        if (!dir.exists() || !dir.isDirectory()) {
+            return null;
+        }
+
+        // find the schemeName.hs file
+        String schemeName = dir.getName();
+        File hsFile = null;
+        for (File file : dir.listFiles()) {
+            if (file.isFile() && file.getName().equals(schemeName + ".hs")) {
+                hsFile = file;
+                break;
+            }
+        }
+        if (hsFile == null) {
+            return null;
+        }
+
+        // read and parse file
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(hsFile));
+            StringBuilder builder = new StringBuilder();
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append('\n');
+            }
+
+            HashingScheme scheme = HashingScheme.fromStorageString(builder.toString());
+            // correct the scheme name
+            if (!scheme.getName().equals(schemeName)) {
+                scheme.setName(schemeName);
+            }
+            return scheme;
+        }
+        catch (FileNotFoundException ex) {
+            // based on what we have done above, this shall never happen
+            return null;
+        }
+        catch (IOException ex) {
+            return null;
+        }
     }
 
-    private void deleteScheme(String name) throws SchemeNotFoundException {
-        //TODO
+    /**
+     * Get all the saved schemes. Will try to delete invalid scheme folders.
+     *
+     * @param context the app context
+     * @return a linkedlist of all saved schemes
+     */
+    public LinkedList<HashingScheme> getAllSchemes(Context context) {
+        LinkedList<HashingScheme> schemes = new LinkedList<HashingScheme>();
+        File sDir = getSchemeDir(context);
+
+        // read all
+        File[] subDirs = sDir.listFiles();
+        for (File subDir : subDirs) {
+            if (subDir.isDirectory()) {
+                HashingScheme scheme = getOneScheme(subDir);
+                if (scheme != null) {
+                    schemes.add(scheme);
+                }
+                else {
+                    // remove invalid
+                    FileUtility.DeleteRecursively(subDir);
+                }
+            }
+        }
+
+        return schemes;
+    }
+
+    /**
+     * Delete a scheme (its folder) given its name.
+     *
+     * @param context the app context
+     * @param name the name of the scheme to delete
+     * @return true if everything works, false if it is found but something go wrong while deleting
+     * @throws SchemeNotFoundException when it can not be found
+     */
+    public boolean deleteScheme(Context context, String name) throws SchemeNotFoundException {
+        File sDir = getSchemeDir(context);
+
+        for (File sub : sDir.listFiles()) {
+            if (sub.isDirectory() && sub.getName().equals(name)) {
+                return FileUtility.DeleteRecursively(sub);
+            }
+        }
+
+        throw new SchemeNotFoundException("Scheme not found for deleteScheme: " + name);
     }
 }
---
+++ app/src/main/java/hk/ust/ustac/team8/generalutility/FileUtility.java
@@ -0,0 +1,36 @@
+package hk.ust.ustac.team8.generalutility;
+
+import java.io.File;
+
+/**
+ * Some static methods that are related to Java File
+ */
+public final class FileUtility {
+
+    /**
+     * Private constructor to prevent creating instance
+     */
+    private FileUtility() {
+
+    }
+
+    /**
+     * Recursively delete the sub file and dirs (if any) of a dir and then itself. Or delete a file.
+     *
+     * @param fileOrDir the root to be deleted
+     * @return true if everything was done successfully, false otherwise
+     */
+    public static boolean DeleteRecursively(File fileOrDir) {
+        boolean subSuccess = true;
+
+        if (fileOrDir.isDirectory()) {
+            for (File sub : fileOrDir.listFiles()) {
+                subSuccess = DeleteRecursively(sub);
+            }
+        }
+
+        boolean thisSuccess = fileOrDir.delete();
+
+        return subSuccess && thisSuccess;
+    }
+}

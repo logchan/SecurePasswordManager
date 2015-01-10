b257958bd804d945e02078e1fb71ce0134038506
logchan <loliconlg@gmail.com>
2015-01-10 09:36:53 +0800

    created AppFileUtility

---
+++ app/src/main/java/hk/ust/ustac/team8/applicationutility/AppFileUtility.java
@@ -0,0 +1,32 @@
+package hk.ust.ustac.team8.applicationutility;
+
+import hk.ust.ustac.team8.exception.SchemeNotFoundException;
+import hk.ust.ustac.team8.hashingscheme.HashingScheme;
+
+import java.util.LinkedList;
+
+/**
+ * Some static methods involving the files of this application
+ */
+public final class AppFileUtility {
+
+    /**
+     * Private constructor to prevent creating instance
+     */
+    private AppFileUtility() {
+
+    }
+
+    private void saveScheme(HashingScheme scheme) {
+        //TODO
+    }
+
+    private LinkedList<HashingScheme> getAllSchemes() {
+        //TODO
+        return null;
+    }
+
+    private void deleteScheme(String name) throws SchemeNotFoundException {
+        //TODO
+    }
+}

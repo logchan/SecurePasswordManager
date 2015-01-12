9e81ad93cbdfae9665bbe0f82432274b0ad94f9d
logchan <loliconlg@gmail.com>
2015-01-12 13:59:34 +0800

    making HashingResultActivity

---
+++ app/src/main/java/hk/ust/ustac/team8/securepasswordmanager/HashingResultActivity.java
@@ -0,0 +1,75 @@
+package hk.ust.ustac.team8.securepasswordmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+
+public class HashingResultActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+    private TextView resultText1;
+
+    private TextView resultText2;
+
+    private SeekBar resultSizeSeek;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hashing_result);
+
+        // setup reference to views
+        resultText1 = (TextView) findViewById(R.id.resultTextView1);
+        resultText2 = (TextView) findViewById(R.id.resultTextView2);
+        resultSizeSeek = (SeekBar) findViewById(R.id.resultSizeSeek);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_hashing_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        switch (seekBar.getId()) {
+            case R.id.resultSizeSeek:
+                resultText1.setTextSize(TypedValue.COMPLEX_UNIT_SP, resultSizeSeek.getProgress());
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // do nothing
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // do nothing
+    }
+}
